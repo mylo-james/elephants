b740f0b2561d89d43ed9a8bd6b5e921579fbabf5
Mylo James <mjames@appacademy.io>
2020-11-12 15:41:57 -0600

    inital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+//import for Class Component
+import { Component } from "react";
+//image imports
+import stuck from "./elephants/stuck.jpg";
+import stick from "./elephants/stick.jpg";
+import suck from "./elephants/suck.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//component imports
+import EleForm from "./EleForm";
+import ElePhoto from "./ElePhoto";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elePhoto: "",
+      setElePhoto: this.setElePhoto,
+      pics: [
+        { name: "Stuck", url: stuck },
+        { name: "Stick", url: stick },
+        { name: "Suck", url: suck },
+      ],
+    };
+  }
+
+  //Method to set elePhoto
+  setElePhoto = (elePhoto) => {
+    this.setState({ elePhoto });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <EleForm pics={this.state.pics} setElePhoto={this.state.setElePhoto} />
+        <ElePhoto elePhoto={this.state.elePhoto} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/EleForm.js
@@ -0,0 +1,11 @@
+import EleInput from "./EleInput";
+function EleForm({ pics, setElePhoto }) {
+  return (
+    <form>
+      {pics.map((pic) => (
+        <EleInput key={pic.name} setElePhoto={setElePhoto} pic={pic} />
+      ))}
+    </form>
+  );
+}
+export default EleForm;
